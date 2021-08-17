e10c9ab19b60302c0c46a5a3a9e65faf9c586e9c
Thiago Muta <thimuta@hotmail.com>
2021-08-17 16:12:58 -0300

    MODELS E CRUD RAILS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # listar todos os tasks
+  get 'tasks', to: 'tasks#index'
+  # mostrar um form de criar
+  get 'tasks/new', to: 'tasks#new'
+  # mostrar um task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # salva o task no db
+  post 'tasks', to: 'tasks#create'
+  # mostrar um form de edit
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+   # atualizar o task no db
+  patch 'tasks/:id', to: 'tasks#update'
+   # deletar um task
+  delete "tasks/:id", to: "tasks#destroy"
 end
